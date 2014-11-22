5e612548ae1a6b34ce08f6a3400eab5f1b93b2af
QuasarDelta <darkgreenzenith@gmail.com>
2014-11-22 16:17:58 +0100

    Contains help commands

---
+++ src/modules/help.py
@@ -0,0 +1,11 @@
+class Helper():
+
+    bbc = "Retrieves the most recent news article from BBC News and posts a summary. Use !bbc random for a" \
+                  "random, recent news story. "
+    roll = "'Rolls' a number between 1 and 100."
+    me = "I'm sorry, I can't help you."
+    t = "Takes two arguments; A and B. Argument A will be the source language, argument B will be the target" \
+                  " language. Languages follow ISO 639-1. (See: http://goo.gl/nVuDQJ )"
+    dt = "Detects the language the sentence is written in and returns a translation in English. No language" \
+                  " codes are required."
+    qdb = "Fetches the quote with the number requested. Lazily coded."
